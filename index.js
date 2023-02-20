d98687fcac058aae97b39a3eb061e1ea6f0e6587
EhSlashEh <anthonyalanb@hotmail.com>
2023-02-20 01:45:05 -0400

    Part way through re-doing header

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+
+
+var coll = document.getElementsByClassName("hamburger-menu");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }
+        else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
